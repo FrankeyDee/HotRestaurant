8ed10c0d0262b42004650f837a04ed7ef2d1ea41
Daria Korbysheva <korbyshevadaria@gmail.com>
2019-09-07 11:41:46 -0700

    added var with Reserv to js

--- server.js
+++ server.js
@@ -17,6 +17,31 @@ app.get("/", function(req, res) {
     res.sendFile(path.join(__dirname, "view.html"));
   });
 
+  var reserv = [
+    {
+      name: "Zac",
+      phoneNumber: 2323232323,
+      email: "zac@aol.com",
+      uniqueId: 1
+    },
+    {
+      name: "Frankey",
+      phoneNumber: 8181818138,
+      email: "frankey@gmail.com",
+      uniqueId: 2
+      
+    },
+    
+  ];
+
+  app.post("/api/view", function(req, res) {
+    var newReserv = req.body;
+    newReserv.routeName = newReserv.name.replace(/\s+/g, "").toLowerCase();
+    console.log(newReserv);
+    characters.push(newReserv);
+    res.json(newReserv);
+  });
+
 
 app.listen(PORT, function() {
     console.log("App listening on PORT " + PORT);
