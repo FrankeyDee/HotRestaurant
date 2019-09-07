30ccf2c4a23aadddeb2b952d441366e625ad4989
Daria Korbysheva <korbyshevadaria@gmail.com>
2019-09-07 12:43:08 -0700

    updated working server

--- server.js
+++ server.js
@@ -4,18 +4,10 @@ var path = require("path");
 var app = express();
 var PORT = 3000;
 
+// Sets up the Express app to handle data parsing
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
-app.get("/", function(req, res) {
-    res.sendFile(path.join(__dirname, "home.html"));
-  });
-  
-  app.get("/reserve", function(req, res) {
-    res.sendFile(path.join(__dirname, "reserve.html"));
-  });
-  
-  app.get("/tables", function(req, res) {
-    res.sendFile(path.join(__dirname, "tables.html"));
-  });
 
   var reserv = [
     {
@@ -34,6 +26,41 @@ app.get("/", function(req, res) {
     
   ];
 
+
+  app.get("/", function(req, res) {
+    res.sendFile(path.join(__dirname, "home.html"));
+  });
+  
+  app.get("/reserve", function(req, res) {
+    res.sendFile(path.join(__dirname, "reserve.html"));
+  });
+  
+  app.get("/tables", function(req, res) {
+    res.sendFile(path.join(__dirname, "tables.html"));
+  });
+
+
+  app.get("/api/reserv", function(req, res) {
+    return res.json(reserv);
+  });
+
+
+  app.get("/api/reserv/:reservation", function(req, res) {
+    var chosen = req.params.reservation;
+  
+    console.log(chosen);
+  
+    for (var i = 0; i < reserv.length; i++) {
+      if (chosen === reserv[i].routeName) {
+        return res.json(reserv[i]);
+      }
+    }
+  
+    return res.json(false);
+  });
+  
+  // Create New Characters - takes in JSON input
+  
   app.post("/api/tables", function(req, res) {
     var newReserv = req.body;
     newReserv.routeName = newReserv.name.replace(/\s+/g, "").toLowerCase();
